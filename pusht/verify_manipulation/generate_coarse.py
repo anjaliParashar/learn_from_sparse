7b2c61e84a89421c324b8ece4375067d86c96fd1
anjaliParashar <anjalip@mit.edu>
2024-07-06 19:11:58 -0400

    1 parameter data update

--- pusht/verify_manipulation/generate_coarse.py
+++ pusht/verify_manipulation/generate_coarse.py
@@ -11,8 +11,13 @@ def generate_dataset(args):
    # l_max=args['l_max']
    # m_max=args['m_max']
    # f_max=args['f_max']
-    I = args['I']
-    J = args['J']
+    I_min = args['I_min']
+    J_min = args['J_min']
+    I_max = args['I_max']
+    J_max = args['J_max']
+    I_n = args['I_n']
+    J_n = args['J_n']
+    M = args['M']
     m1 =1
     l_min,l_max,f_min,f_max = 4,8,0.1,1
     seed_list = []
@@ -20,16 +25,16 @@ def generate_dataset(args):
     mass_list  =[]
     length_list = []
     friction_list  =[]
-    score_ = np.zeros((3,3,5,5))
+    score_ = np.zeros((3,3,I_n,J_n))
     l_idx = 0
     timer=0
     for l1 in np.linspace(l_min,l_max,3):
         f_idx = 0
         for f1 in np.linspace(f_min,f_max,3):
             i_idx=0
-            for i in np.linspace(I,I+4,5):
+            for i in np.linspace(I_min,I_max,I_n):
                 j_idx=0
-                for j in np.linspace(J,J+4,5):
+                for j in np.linspace(J_min,J_max,J_n):
                     #i in range(1000):
                     pusht = inference_pusht(int(i),int(j),m1,f1,l1)
                     obs0 = pusht.init_obs
@@ -53,7 +58,7 @@ def generate_dataset(args):
                     
         
     data = {'seed':seed_list,'length':length_list,'friction':friction_list,'score':score_}
-    filename='data/pusht_train_'+str(I)+str(J)+'.pkl'
+    filename='data/pusht_train_'+str(M)+'.pkl'
     file = open(filename, 'wb')
 
     # dump information to that file
@@ -63,7 +68,12 @@ def generate_dataset(args):
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--I", type=int)
-    parser.add_argument("--J", type=int)
+    parser.add_argument("--I_min", type=int)
+    parser.add_argument("--J_min", type=int)
+    parser.add_argument("--I_max", type=int)
+    parser.add_argument("--J_max", type=int)
+    parser.add_argument("--I_n", type=int)
+    parser.add_argument("--J_n", type=int)
+    parser.add_argument("--M", type=int)
     args = vars(parser.parse_args())
     generate_dataset(args)
